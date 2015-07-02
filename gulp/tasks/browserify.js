c4e28af36f76ad73ad0b218974b24652c5c1c710
Jacob Sin <jacob.sin@gmail.com>
2015-07-03 01:03:51 +0800

    browserify to work with sourcemap

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var errorHandler = require('../common/error-handler');
 var bundleLogger = require('../common/bundle-logger');
 
@@ -10,11 +11,15 @@ gulp.task('browserify', ['scripts', 'scripts-coffee'], function() {
         entries: ['app/scripts/angular-main.coffee'],
         extensions: ['.coffee'],
         outputName: 'angular-bundle.js',
-        dest: '.tmp/scripts'
+        dest: '.tmp/scripts',
+        debug: true
     };
     bundleLogger.start(bundleConfig.outputName);
     return browserify(bundleConfig)
         .bundle().on('error', errorHandler.process)
         .pipe(source(bundleConfig.outputName))
+        .pipe(buffer())
+        .pipe($.sourcemaps.init({loadMaps: true}))
+        .pipe($.sourcemaps.write('./maps'))
         .pipe(gulp.dest(bundleConfig.dest));
 });
